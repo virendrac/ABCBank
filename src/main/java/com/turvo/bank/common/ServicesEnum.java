0759d52529677ed9d027b401469201201f7d3dcd
virendrac <vcvirus007@gmail.com>
2018-02-28 10:21:31 +0530

    Fixed the services and checking the validity of selected services before creating a Token.

--- src/main/java/com/turvo/bank/common/ServicesEnum.java
+++ src/main/java/com/turvo/bank/common/ServicesEnum.java
@@ -1,7 +1,7 @@
 package com.turvo.bank.common;
 
 public enum ServicesEnum implements SuperEnum{
-    DEPOSIT("DEPOSIT") ,WITHDRAWL("DEPOSIT"),ACCOUNTOPENING("ACCOUNTOPENING"),LOAN("LOAN"),OTHER("OTHER");
+    DEPOSIT("DEPOSIT") ,WITHDRAWL("WITHDRAWL"),ACCOUNTOPENING("ACCOUNTOPENING"),LOAN("LOAN"),OTHER("OTHER");
     private String value;
 
     ServicesEnum(String service) {
